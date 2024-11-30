84ebcbf8784a3c50fa86de43df29bd2467d67cf9
muskanshah1804 <muskanshah18004@gmail.com>
2024-11-30 20:26:44 +0530

    Add files via upload
    Signed-off-by: muskanshah1804 <muskanshah18004@gmail.com>

---
+++ UserRepository.java
@@ -0,0 +1,9 @@
+package com.ethicalhackingplatform.repositories;
+
+import com.ethicalhackingplatform.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
